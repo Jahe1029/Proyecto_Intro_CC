f5a08282408d00dd651956cf50dd440bb2443d99
Jahe1029 <hesoyamreddit@gmail.com>
2026-05-09 21:25:18 -0500

    test clase Cuerpos

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import numpy as np
 class cuerpo:
-	def __init__(self, posicion, velocidad, acelerción):
+	def __init__(self, posicion : np.array, velocidad: np.array, acelerción: np.array):
 		self.r = posicion
 		self.v = velocidad
 		self.a = acelerción
+	
+	def cout(self):
+		print(self.r)
+		print(self.v)
+		print(self.a)
+	
+	def RK4(self, c : cuerpo):
+		print(np.sum(self.r + c.r))
 
-def yo_que_se(number):
-	return number + 1
-
-print(yo_que_se(21))
+def main():
+	tierra = cuerpo([1, 2, 2], [0, 0, 0], [4, 5, 4])
+	# tierra.cout()
+	marte = cuerpo([1, 2, 2], [0, 0, 0], [4, 5, 4])
+	tierra.RK4(marte)
+main()
