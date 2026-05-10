138e7aef75303b2278868f5fcfbcd9a720b59146
Jahe1029 <hesoyamreddit@gmail.com>
2026-05-10 00:45:43 -0500

    se creó el html y js

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 class cuerpo:
 	def __init__(self, posicion : np.array, velocidad: np.array, acelerción: np.array):
@@ -12,10 +13,18 @@ class cuerpo:
 	
 	def RK4(self, c : cuerpo):
 		print(np.sum(self.r + c.r))
-
+def save_positions():
+	with open("tierra.txt", "w") as f:
+		for i in range(10):
+			f.write(str(random.randint(1, 100)))
+			f.write(",")
+			f.write(str(random.randint(1, 100)))
+			f.write("\n")
 def main():
 	tierra = cuerpo([1, 2, 2], [0, 0, 0], [4, 5, 4])
 	# tierra.cout()
 	marte = cuerpo([1, 2, 2], [0, 0, 0], [4, 5, 4])
-	tierra.RK4(marte)
+	save_positions()
 main()
+
+
