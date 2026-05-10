8096fe13cc47acbcca97692170da0a46835bb32f
Jahe1029 <hesoyamreddit@gmail.com>
2026-05-09 20:31:38 -0500

    2 test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import numpy as np
+class cuerpo:
+	def __init__(self, posicion, velocidad, acelerción):
+		self.r = posicion
+		self.v = velocidad
+		self.a = acelerción
+
 def yo_que_se(number):
 	return number + 1
 
